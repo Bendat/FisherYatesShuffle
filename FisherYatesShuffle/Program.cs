0e5ea48275d949c6eec44d45aa7d60c24af1a1da
Ben Aherne <bta@outlook.ie>
2015-07-22 17:23:58 -0400

    Created repo

---
+++ FisherYatesShuffle/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FisherYatesShuffle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayShuffle shuffle = new ArrayShuffle();
+            int[] array = {1, 3, 5, 2, 8, 9, 0};
+            Console.Out.WriteLine("Input list: " + ArrayToString(array));
+            Console.Out.WriteLine("Shuffling...");
+            shuffle.Shuffle(array);
+            Console.Out.WriteLine("Output list: " + ArrayToString(array));
+            Console.WriteLine("\n***************List Test****************\n");
+            List<int> list = new List<int>(array);
+            Console.Out.WriteLine("Input list: " + ListToString(list));
+            Console.Out.WriteLine("Shuffling...");
+            shuffle.Shuffle(list);
+            Console.Out.WriteLine("Output list: " + ListToString(list));
+            Console.ReadKey();
+
+        }
+
+        public static string ArrayToString<T>(T[] array, string delim = " ")
+        {
+            string result = "";
+            foreach (var item in array)
+            {
+                result += item.ToString();
+                result += delim;
+            }
+            return result;
+        }
+        public static string ListToString<T>(List<T> list, string delim = " ")
+        {
+            string result = "";
+            foreach (var item in list)
+            {
+                result += item.ToString();
+                result += delim;
+            }
+            return result;
+        }
+
+    }
+}
---
+++ FisherYatesShuffle/Shuffle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FisherYatesShuffle
+{
+    class ArrayShuffle
+    {
+        private static Random _swap;
+
+        public ArrayShuffle()
+        {
+            _swap = new Random();   
+        }
+
+        public void Shuffle<T>(T[] array)
+        {
+            int length = array.Length;
+            for (int index = 0; index < length; index++)
+            {
+                int current = _swap.Next(index, length);
+                T temp = array[index];
+                array[index] = array[current];
+                array[current] = temp;
+            }
+        }
+
+        public void Shuffle<T>(List<T> list)
+        {
+            T[] array = list.ToArray();
+            Shuffle(array);
+            list.Clear();
+            list.AddRange(array);
+        }
+    }
+}
